69313ac8b135ccf2232acdcaed3684d157f30f5a
Ferry <FerryRietveld@live.nl>
2026-01-30 19:04:52 +0200

    Added input validation for type

--- app/models.py
+++ app/models.py
@@ -1,24 +1,29 @@
 from sqlmodel import Field, SQLModel
 from datetime import date
+from enum import Enum
+
+class TransactionType(str, Enum):
+    income = "income"
+    expense = "expense"
 
 class Transaction(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
     description: str | None
-    type: str
+    type: TransactionType
     amount: float
     transaction_date: date
 
 class TransactionCreate(SQLModel):
     name: str
     description: str | None
-    type: str
+    type: TransactionType
     amount: float
     transaction_date: date
 
 class TransactionUpdate(SQLModel):
     name: str | None = None
     description: str | None = None
-    type: str | None = None
+    type: TransactionType | None = None
     amount: float | None = None
     transaction_date: date | None = None
