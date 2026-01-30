c0b47b94684fadfeda22dd1bab1732bfbc516d3a
Ferry <FerryRietveld@live.nl>
2026-01-30 17:18:46 +0200

    Added model for transaction db

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from sqlmodel import Field, SQLModel
+from datetime import date
+
+class Transaction(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    description: str | None
+    type: str
+    amount: float
+    transaction_date: date
