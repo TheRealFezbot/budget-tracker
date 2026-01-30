55915cf37ee9624242f725214a16553e5d3d8ad3
Ferry <FerryRietveld@live.nl>
2026-01-30 17:49:59 +0200

    app setup with database init working

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.database import create_db_and_tables
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
